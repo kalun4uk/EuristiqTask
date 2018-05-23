4ef901d2b26be482295d1ae37e411eef0f3a8eee
kalun4uk <34134819+kalun4uk@users.noreply.github.com>
2018-05-23 13:53:42 +0300

    Add files via upload

---
+++ ProjectTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectLibrary;
+
+namespace ProjectTests
+{
+    /// <summary>
+    /// the sinplest verification unit test for example
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Actions action = new Actions(new Data(0, 0));
+            double expected = 0;
+            double actual = action.Calculation();
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
