a6ad8f55d2838a5f4e9ca634677aefcb36c127c8
kalun4uk <34134819+kalun4uk@users.noreply.github.com>
2018-05-23 13:52:40 +0300

    Add files via upload

---
+++ Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ProjectLibrary;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter BS - Basic Salary ");
+                double bS = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter CS - Current Salary ");
+                double cS = double.Parse(Console.ReadLine());
+                Actions action = new Actions(new Data(bS, cS));
+                action.Show();
+            }
+            catch (FormatException) { }
+            Console.ReadKey();
+        }
+    }
+}
